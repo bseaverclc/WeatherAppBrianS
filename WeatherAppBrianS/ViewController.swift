3541f99b8362f07f09c8d0eced60e18ff203228b
Brian Seaver <bseaver@d155.org>
2020-10-11 18:33:32 -0500

    Initial Commit

---
+++ WeatherAppBrianS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherAppBrianS
+//
+//  Created by Brian Seaver on 10/11/20.
+//  Copyright © 2020 clc.seaver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
